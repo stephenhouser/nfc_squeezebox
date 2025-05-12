7f584054c6002fe5c22081634874a5cb0f210512
Stephen Houser <stephenhouser@gmail.com>
2025-05-11 22:05:49 -0400

    add plugin, like I thought I did

--- slimserver/Plugins/RFIDControl/Plugin.pm
+++ slimserver/Plugins/RFIDControl/Plugin.pm
@@ -52,6 +52,8 @@ my $log = Slim::Utils::Log->addLogCategory({
 	'description' => 'PLUGIN_RFIDCONTROL',
 });
 
+my $tags;
+
 sub initPlugin {
 	my $class = shift;
 	$class->SUPER::initPlugin(@_);
@@ -68,6 +70,13 @@ sub initPlugin {
 	# Client, Query, Tags
 	Slim::Control::Request::addDispatch(['rfid', 'pair', '_readerid', '?'], [0, 1, 0, \&rfidPairReader]);
 	Slim::Control::Request::addDispatch(['rfid', 'tag', '_tagid'], [1, 0, 0, \&rfidTagChanged]);
+
+	# read tags.csv
+	$tags = load_csv_to_hash(
+    	filename    => '/config/cache/Plugins/RFIDControl/tags.csv',
+    	key_columns => [0],    # Use first column as key; change to [0,1] for composite keys
+    	has_header  => 1       # Set to 1 if the CSV has a header row
+	);
 }
 
 sub postinitPlugin {
@@ -155,25 +164,44 @@ sub rfidTagChanged {
 		return;
 	}
 
-	# if (!defined ($filters->{$filterId})) {
-	# 	$log->warn("Unknown filter $filterId");
-	# 	$request->setStatusBadParams();
-	# 	return;
-	# }
-	$log->error('_tagid '.$tagId.' received');
-	if ($tagId eq 'stop') {
-		my $playRequest = Slim::Control::Request::executeRequest($client, ['stop']);
+	$log->error('tag:'.$tagId.' received for client:'.$client->id());
+
+	# Sort out what to do with the tag...
+	my $tagCmd = $tags->{uc($tagId)};
+	if ($tagCmd) {
+		my $cmd = $tagCmd->[1];
+		if ($cmd eq 'command') {
+			my @cmdParameters = split(/ /, $tagCmd->[2]);
+			my $request = Slim::Control::Request::executeRequest($client, ['playlist', 'clear']);
+		} elsif ($cmd eq 'url') {
+			my $url = $tagCmd->[2];
+			my $request = Slim::Control::Request::executeRequest($client, ['playlist', 'play', $url]);
+		} elsif ($cmd eq 'album') {
+			my $album = '/music/'.$tagCmd->[2].'/';
+			my $request = Slim::Control::Request::executeRequest($client, ['playlist', 'play', $album]);
+		} elsif ($cmd eq 'artist') {
+			my $artist = $tagCmd->[2];
+			my $request = Slim::Control::Request::executeRequest($client, ['dynamicplaylist', 'playlist', 'play', 'dplccustom_play_artist', 'dynamicplaylist_parameter_1:'.$artist]);
+		} elsif ($cmd eq 'genre') {
+			my $genre = $tagCmd->[2];
+			my $request = Slim::Control::Request::executeRequest($client, ['dynamicplaylist', 'playlist', 'play', 'dplccustom_play_genre', 'dynamicplaylist_parameter_1:'.$genre]);
+		} elsif ($cmd eq 'year') {
+			my $year = $tagCmd->[2];
+			my $request = Slim::Control::Request::executeRequest($client, ['dynamicplaylist', 'playlist', 'play', 'dplccustom_play_year', 'dynamicplaylist_parameter_1:'.$year]);
+		} elsif ($cmd eq 'decade') {
+			my $decade = $tagCmd->[2];
+			my $request = Slim::Control::Request::executeRequest($client, ['dynamicplaylist', 'playlist', 'play', 'dplccustom_play_decade', 'dynamicplaylist_parameter_1:'.$decade]);
+		} else {
+			$log->warn('Unknown tag command:' . $cmd . ': ' . $tagCmd->[2]);
+			$request->setStatusBadParams();
+			return;
+		}
+	} else {
+		$log->warn('No entry for tag '.$tagId);
+		$request->setStatusBadParams();
+		return;
 	}
 
-	if ($tagId eq 'prince') {
-		my $playRequest = Slim::Control::Request::executeRequest($client, ['playlist', 'play', '/music/Prince/1999 (1982)/']);
-	}
-	
-	if ($tagId eq '100.9') {
-		my $playRequest = Slim::Control::Request::executeRequest($client, ['playlist', 'play', 'http://opml.radiotime.com/Tune.ashx?id=s24232&formats=aac,ogg,mp3&partnerId=15&serial=59bf631fddda17f8c19e2bc4914096f1']);
-	}
-
-	#$request->addResult('tag', $tagId);
 	$request->setStatusDone();
 }
 
@@ -239,6 +267,73 @@ sub getOverlay {
 	return [undef, $client->symbols('rightarrow')];
 }
 
+
+## CSV 
+
+sub load_csv_to_hash {
+    my (%args) = @_;
+    my $filename    = $args{filename} or die "Filename is required.\n";
+    my $key_columns = $args{key_columns} || [0];  # Default to first column as key
+    my $has_header  = $args{has_header} || 0;
+
+    my %csv_hash;
+
+    open my $fh, '<', $filename or die "Cannot open $filename: $!";
+
+    # Read header if present
+    my @header;
+    if ($has_header) {
+        my $header_line = <$fh>;
+        chomp $header_line;
+        @header = split_csv_line($header_line);
+    }
+
+    while (my $line = <$fh>) {
+        chomp $line;
+        next if $line =~ /^\s*$/;  # Skip empty lines
+
+        my @fields = split_csv_line($line);
+
+        # Build the key from specified columns
+        my $key = join('|', map { $fields[$_] } @$key_columns);
+
+        # Store the entire row as an array reference
+        $csv_hash{uc($key)} = \@fields;
+    }
+
+    close $fh;
+    return \%csv_hash;
+}
+
+sub split_csv_line {
+    my ($line) = @_;
+
+    # Simple CSV parser: handles commas and quoted fields
+    my @fields;
+    while (length $line) {
+        if ($line =~ s/^"((?:[^"]|"")*)"[, ]*//) {
+            # Quoted field, handle escaped quotes
+            my $field = $1;
+            $field =~ s/""/"/g;
+            push @fields, $field;
+        } elsif ($line =~ s/^([^,]*),\s*//) {
+            # Unquoted field
+            push @fields, $1;
+        } else {
+            # Last field (quoted or unquoted)
+            if ($line =~ s/^"(.*)"\s*$//) {
+                my $field = $1;
+                $field =~ s/""/"/g;
+                push @fields, $field;
+            } else {
+                push @fields, $line;
+            }
+            last;
+        }
+    }
+
+    return @fields;
+}
 
 *escape = \&URI::Escape::uri_escape_utf8;
 *unescape = \&URI::Escape::uri_unescape;
